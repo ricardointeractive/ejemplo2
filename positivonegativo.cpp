892ff38340f75b223ed13a4aa81ee3b67b3ff912
unknown <ricardointeractive@hotmail.com>
2013-09-02 18:59:16 -0500

    segundo cambio

---
+++ positivonegativo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char *argv[])
+	{
+	int numero;
+	printf("Inserte su numero: ");
+	scanf("%d",&numero);
+	if(numero==0)
+	printf("El numero %d es NULO",numero);
+	else
+		{
+			if(numero<0)
+				printf("El numero %d es NEGATIVO",numero);
+			else
+				printf("El numero %d es POSITIVO",numero);
+		}
+	return 0;
+}
+
